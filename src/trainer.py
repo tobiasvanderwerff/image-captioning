876fe1a0a68200988480782dd34c1237cc3e8232
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2021-03-30 23:32:01 +0200

    Model without attention finally works, bug in collate_fn function resulted in wrong image-caption pairs

--- src/trainer.py
+++ src/trainer.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from src.utils import plot_grad_flow
 
 logger = logging.getLogger(__name__)
 
@@ -60,11 +61,12 @@ class Trainer:
             for data in dataloader:
                 model.train(is_train)  # put model in training or evaluation mode
 
-                data = [el.to(self.device) for el in data]  # put data on the appropriate device (cpu or gpu)
+                # put data on the appropriate device (cpu or gpu)
+                data = [el.to(self.device) if hasattr(el, 'device') else el for el in data]  
 
                 optimizer.zero_grad()  # set the gradients to zero
 
-                logits, loss, num_correct, num_samples = model(*data)  # forward pass
+                logits, loss, num_correct, num_samples, _ = model(*data)  # forward pass
                 
                 losses.append(loss.item())
                 if config.track_loss:
@@ -90,12 +92,7 @@ class Trainer:
 
         for ep in range(config.epochs):
             run_epoch('train')
+#             plot_grad_flow(model.named_parameters())
             if self.eval_ds is not None:
                 with torch.no_grad():
                     run_epoch('eval')
-                    
-    @torch.no_grad()
-    def _track_grad_norm(self):
-        """ Calculate and store the 2-norm of the gradients in the model. """
-        grad_norm = np.sqrt(np.sum(LA.norm(p.grad.cpu().numpy()) ** 2 for p in self.model.parameters() if p.requires_grad))
-        self.grad_norms.append(grad_norm)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ from functools import partial
 
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -13,9 +16,11 @@ def collate_fn(batch):
     customizes the default behavior by adding padding of the captions. It pads each caption to the length 
     of the longest caption in the batch. 
     """
-    imgs, captions, targets = zip(*batch)
-    captions = sorted(captions, reverse=True, key=lambda seq: len(seq))
-    targets = sorted(targets, reverse=True, key=lambda seq: len(seq))
+    imgs, captions, targets, split = zip(*batch)
+    ordering, _ = zip(*sorted(enumerate(captions), reverse=True, key=lambda it: len(it[1])))
+    captions = [captions[i] for i in ordering]
+    targets = [targets[i] for i in ordering]
+    imgs = [imgs[i] for i in ordering]
     seq_lengths = [len(seq) for seq in captions]
     max_seq_len = max(seq_lengths)
 
@@ -31,4 +36,38 @@ def collate_fn(batch):
     imgs = torch.stack(imgs, 0)
     captions_pad, seq_lengths, targets_pad = map(to_long_tensor, (captions_pad, seq_lengths, targets_pad))
     
-    return imgs, captions_pad, seq_lengths, targets_pad
+#     return imgs, captions_pad, seq_lengths, targets_pad
+    return imgs, split, captions_pad, seq_lengths, targets_pad
+
+
+def plot_grad_flow(named_parameters):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+    
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow
+    
+    Taken from https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/8 '''
+
+    ave_grads = []
+    max_grads= []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n) and ("encoder.resnet" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
+    plt.xticks(range(0 ,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02) # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+    plt.show()
